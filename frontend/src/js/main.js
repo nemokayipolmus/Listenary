1f8a10ac744dec1216aa4e29b98d13d0c60446ae
Alper Dönmez <44973133+nemokayipolmus@users.noreply.github.com>
2022-06-22 16:10:00 +0300

    Add files via upload

---
+++ frontend/src/js/main.js
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+const footer = document.getElementById('footer');
+const navbar = document.getElementById('navbar');
+
+export default() => {
+    Setup();
+
+}
+
+
+
+function Setup(){
+// footer.innerHTML = Footer.SetupFooter();
+navbar.innerHTML = Header.SetupNavBar()
+Header.SetupEventListeners();
+}
